d7dd61f0a84461851e10aeea5bc9d0227720d2d4
luis arthur <luisarthurlards03@gmail.com>
2022-05-12 15:34:39 -0300

    renomeamento

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ const roman_numbers={
     'c':100,
     'd':500,
     'm':1000,
-    'v':5000,
+    'v_':5000,
     'x_':1000,
     'l_':50000,
     'c_':100000,
 }
-const number='ix'
+const numbero='ix'
 
 let somatorio=0;
-const sumer = (i)=>{somatorio+=roman_numbers[number[i]]; return somatorio;}
+const sumer = (x,i)=>{somatorio+=roman_numbers[x[i]]; return somatorio;}
 
+const transcribe = (number)=>{
+    console.log(number)
     for(let i=0;i<=number.length-1;i++){
 
         var act_let = roman_numbers[number[i]];
-        if(i==0){sumer(i)}
+        if(i==0){sumer(number,i);}
         else {
             var ant_let = roman_numbers[number[i-1]];
             if(act_let > ant_let){
-                sumer(i);
+                sumer(number,i);
             somatorio-=2*ant_let;
             console.log("ant_let=" + ant_let);
-            }else{sumer(i); console.log("im here bro");}
+            }else{sumer(number,i); console.log("im here bro");}
         }
-
-        console.log(i);
-        console.log(somatorio);
-
     }
-
+    return somatorio;
+}
 
 console.log(roman_numbers['x']);
+console.log(transcribe('xiv'));
