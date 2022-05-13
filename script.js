51e569fdb1d3fa27f327f39d293c2d95f6e62577
luis arthur <luisarthurlards03@gmail.com>
2022-05-13 20:51:48 -0300

    estudo com mudança de cor

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log('a');
-document.getElementById("aa").innerHTML
+const cores=['red','green','blue'];
+var i=0
+const div_de_cima = document.querySelector(".emergency-tasks div");
+function change_color(){
+    div_de_cima.style.background=cores[i%3]
+    i+=1;
+}
+window.setInterval(change_color,10000);
