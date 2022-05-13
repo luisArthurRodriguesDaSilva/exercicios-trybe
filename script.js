2626fa1ddf6689102f17876cb34a5bb8ccadf657
luis arthur <luisarthurlards03@gmail.com>
2022-05-13 15:50:11 -0300

    error with the await sleep

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('a');
+
+const sleep=(ms) =>{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+        while (true)
+        {
+        await sleep(1 * 1000);
+            (document.getElementById("aa").     innerHTML) = 77;
+        console.log(document.getElementById("aa")       .innerHTML);
+        
+        
+        
+        (document.getElementById("aa").     innerHTML) = 777;
+        
+        console.log(document.getElementById("aa")       .innerHTML);
+        }
