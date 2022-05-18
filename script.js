2118c811698c2976cbf7f8259c7e52ec1525063f
luis arthur <luisarthurlards03@gmail.com>
2022-05-17 23:14:39 -0300

    mudando cor

--- script.js
+++ script.js
@@ -32,10 +32,21 @@ input.addEventListener('input',changeTextOfTech);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+const goToMyPage = () =>{
+  window.open('https://github.com/luisArthurRodriguesDaSilva')
+}
+myWebpage.addEventListener('dblclick',goToMyPage)
 //myWebpage.addEventListener('dblclick',goToMyPortfolio)
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
+const changeColor = (e) =>{
+  e.target.style.color='red';
+}
+const returnColor = (e) =>{
+  e.target.style.color='white';
+}
+myWebpage.addEventListener('mouseover',changeColor);
+myWebpage.addEventListener('mouseleave',returnColor);
 // Segue abaixo um exemplo do uso de event.target:
 
 
