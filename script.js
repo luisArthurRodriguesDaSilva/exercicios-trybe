978ebd5a8fbc1dc4fa91e69c336f97f5c03b18c8
luis arthur <luisarthurlards03@gmail.com>
2022-05-19 19:24:50 -0300

    andou um pouco

--- script.js
+++ script.js
@@ -15,12 +15,25 @@ function createDaysOfTheWeek() {
 };
 
 function createDaysOfThedaysOfTheWeek() {
-const weekDaysListobjs = document.querySelectorAll('.week-days li')
-  for (let i in weekDaysListobjs){
-    const ul_days =  document.createElement('ul');
-    ul_days.classList = weekDays[i] + '-days-list'; 
-    weekDaysListobjs[i].appendChild(ul_days);
-  }
+  const position = document.querySelectorAll('.week-days li'); 
+  for (let i of position){
+    const create_ul = document.createElement('ul');
+    i.appendChild(create_ul)
+}
+const position2 = document.querySelectorAll('.week-days li ul');
+for (let i of position2){
+  const create_li = document.createElement('li');
+  i.appendChild(create_li)
+}
+for (let i of position2){
+  for(let k of dezDaysList){
+      const create_li = document.createElement('li');
+      create_li.innerHTML=k;
+      i.appendChild(create_li)
+  }  
+}
+numeros = document.querySelectorAll('.week-days li ul li')
+for (let num  of numeros){num.style.display = "block"}
   
 }
 
